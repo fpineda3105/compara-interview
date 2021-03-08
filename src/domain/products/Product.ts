e7139cb16a10baf770a596136aaa4fab64cebc86
Fernando Pineda <fernandopineda3105@gmail.com>
2021-03-08 17:24:16 -0500

    Added -> a BaseProduct

---
+++ src/domain/products/Product.ts
@@ -0,0 +1,17 @@
+export interface Product {
+    name: ProductName,
+    sellIn: number,
+    price: number,
+
+    dailyUpdate(): void;    
+
+}
+
+export enum ProductName {
+    FULL_COVERAGE = 'Full Coverage',
+    MEDIUM_COVERAGE = 'Medium Coverage',
+    MEGA_COVERAGE = 'Mega Coverage',
+    SPECIAL_FULL_COVERAGE = 'Special Full Coverage',
+    LOW_COVERAGE = 'Low Coverage',
+    SUPER_SALE = 'Super Sale'
+}
