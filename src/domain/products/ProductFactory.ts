cbe787f38ccd2b26662b1e3d835af4ab69afc26f
Fernando Pineda <fernandopineda3105@gmail.com>
2021-03-09 12:33:03 -0500

    Update - Continuous integration testing coverage

--- src/domain/products/ProductFactory.ts
+++ src/domain/products/ProductFactory.ts
@@ -7,9 +7,7 @@ import { Product, ProductName } from "./Product";
 export class ProductFactory {
 
     static create(name: ProductName, sellIn: number, price: number): Product {
-        switch (name) {             
-            case ProductName.MEGA_COVERAGE:
-                return new MegaCoverageProduct(sellIn, price);
+        switch (name) {                        
             case ProductName.LOW_COVERAGE:
                 return new LowCoverageProduct(sellIn, price);
             case ProductName.MEDIUM_COVERAGE: 
