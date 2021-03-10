2b07d7e4debc5d6ba6506e86c1d3a4b49f66dd65
Fernando Pineda <fernandopineda3105@gmail.com>
2021-03-09 20:28:53 -0500

    Added -> Script after-30-days

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,27 @@ import { ProductFactory } from "./domain/products/ProductFactory";
 import { TxtExtractorSummary } from "./util/TxtExtractorSummary";
 
 const products = [
-    ProductFactory.create(ProductName.MEDIUM_COVERAGE, 10, 20),  
+  ProductFactory.create(ProductName.MEDIUM_COVERAGE, 10, 20),
   ProductFactory.create(ProductName.FULL_COVERAGE, 2, 0),
-    ProductFactory.create(ProductName.LOW_COVERAGE, 5, 7),
-    ProductFactory.create(ProductName.MEGA_COVERAGE, 0, 80),
-    ProductFactory.create(ProductName.MEGA_COVERAGE, -1, 80),
-    ProductFactory.create(ProductName.SPECIAL_FULL_COVERAGE, 15, 20),
-    ProductFactory.create(ProductName.SPECIAL_FULL_COVERAGE, 10, 49),
-    ProductFactory.create(ProductName.SPECIAL_FULL_COVERAGE, 5, 49),
-    ProductFactory.create(ProductName.SUPER_SALE, 3, 6),
+  ProductFactory.create(ProductName.LOW_COVERAGE, 5, 7),
+  ProductFactory.create(ProductName.MEGA_COVERAGE, 0, 80),
+  ProductFactory.create(ProductName.MEGA_COVERAGE, -1, 80),
+  ProductFactory.create(ProductName.SPECIAL_FULL_COVERAGE, 15, 20),
+  ProductFactory.create(ProductName.SPECIAL_FULL_COVERAGE, 10, 49),
+  ProductFactory.create(ProductName.SPECIAL_FULL_COVERAGE, 5, 49),
+  ProductFactory.create(ProductName.SUPER_SALE, 3, 6),
 ];
 const carInsurance = new CarInsurance(products);
 
-function main(): void {  
-  const extractor = new TxtExtractorSummary(carInsurance, "products_after_30_days.txt");
-    extractor.executeForDays(30);
-    console.log("Lets check: products_after_30_days.txt")
+function main(): void {
+  const extractor = new TxtExtractorSummary(
+    carInsurance,
+    "products_after_30_days.txt"
+  );
+  extractor.executeForDays(30);
+  console.log("Lets check: products_after_30_days.txt");
 }
 
-if (process.argv.includes('main')) {
-    main();
- }
+if (process.argv.includes("main")) {
+  main();
+}
